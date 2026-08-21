e1badfd5a02e25f31b63ae7a38fde6a48672015b
gulfcode-cn <2563348897@qq.com>
2026-08-21 22:25:24 +0800

    inital commit

---
+++ nvim/init.lua
@@ -0,0 +1,16 @@
+----------- GUI
+--|
+--| 显示行数
+    vim.opt.number = true
+--| 显示相对行数
+    vim.opt.relativenumber = true
+--| 彩色
+    vim.opt.termguicolors = true
+
+----------- Ident
+--|
+    vim.opt.tabstop = 4
+--|
+    vim.opt.shiftwidth = 4
+--|
+    vim.opt.expandtab = true
